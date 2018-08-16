8fd5800a090933854a2d9ce30475a9be21ae6e7b
Christine Xiang <yutingcxiang@gmail.com>
2018-08-16 20:06:55 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,16 @@ class App < Sinatra::Base
 	get '/' do
 		erb :index
 	end
-
-
+	
+	get '/hello' do
+	  erb :hello
+	end
+	
+	get '/goodbye' do
+    erb :goodbye
+  end
+  
+  get '/date'do
+    erb :date
+  end
 end
